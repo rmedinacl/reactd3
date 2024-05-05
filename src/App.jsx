1ff5064a85ac37e12a3ddaf8b96a0ed524fc0ae4
Rodrigo Medina P <rmedina@mountain-creative.com>
2024-05-04 21:41:58 -0400

    app nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-import Tareas from "./components/Tareas";
 import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import BusquedaCol from "./components/Busqueda";
+import Registro from "./components/Registro";
 import { BaseColaboradores } from "./database/data";
-import "./App.css";
+import TableUno from "./components/Table1";
 
-export default function App() {
-  return <div>Tarea</div>;
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [formErrors, setFormErrors] = useState("");
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+
+  const filteredColaboradores = colaboradores.filter((colaborador) =>
+    Object.values(colaborador).some((val) =>
+      val.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
+
+  const handleUser = (formData) => {
+    setColaboradores([...colaboradores, formData]);
+  };
+
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+  };
+
+  return (
+    <Container fluid="md">
+      <BusquedaCol onSearch={handleSearch} />
+
+      <TableUno searchTerm={searchTerm} data={filteredColaboradores} />
+      <Registro
+        handleUser={handleUser}
+        setFormErrors={setFormErrors}
+        formErrors={formErrors}
+        colaboradores={colaboradores}
+        setColaboradores={setColaboradores}
+      />
+    </Container>
+  );
 }
+
+export default App;
